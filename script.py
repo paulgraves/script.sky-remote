86892ed0a5bf19f142008f4bd18a6ead86d9afd0
Paul Graves <paulgraves@claymoreminds.com>
2018-01-01 13:32:11 +0000

    added scripts

---
+++ script.py
@@ -0,0 +1,29 @@
+import subprocess
+import sys
+import urlparse
+
+skyIp = '192.168.0.2'
+
+params = urlparse.parse_qs('&'.join(sys.argv[1:]))
+command = params.get('command', None)[0]
+	
+		
+if command == 'bbc1':
+	command = '1 1 5'
+elif command == 'bbc2':
+	command = '1 0 2'
+elif command == 'channel4':
+	command = '2 2 7'
+elif command == 'info':
+	command = 'i'
+elif command == 'tvguide':
+    command = 'tvguide tvguide down'	
+elif command == 'pagedown':
+	command = 'down down down down down down down down'
+elif command == 'pageup':
+	command = 'up up up up up up up up'
+	
+cliString = 'sky-remote-cli {} {}'.format(skyIp, command)	
+print cliString
+
+subprocess.check_call(cliString, shell=True)
